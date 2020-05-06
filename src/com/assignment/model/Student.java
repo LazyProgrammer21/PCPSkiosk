492df3a2e6d2e566210d5b869da6fc603c6b47e1
LazyProgrammer <tamang.subash36@gmail.com>
2020-05-06 12:41:39 +0545

    added model student

---
+++ src/com/assignment/model/Student.java
@@ -0,0 +1,25 @@
+package com.assignment.model;
+
+import java.sql.Blob;
+import java.sql.Date;
+
+public class Student {
+	
+	private int id;
+	private String fname;
+	private String Lname;
+	private Blob photo;
+	private Date dob;
+	private String Gender;
+	private String Add_city;
+	private String Add_state;
+	private String Add_district;
+	private String password;
+	private String zipcode;
+	private int phonenumber;
+	private String status; //if a student has booked a event or not;
+	
+	
+	
+
+}
